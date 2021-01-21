3f01eb5156a3348dbf097f117ddc435547cd1446
dobrowj1 <01142560@pw.edu.pl>
2021-01-21 04:38:27 +0100

    Encoding fix for windows

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-f = open("hashtags.txt", "r")
+f = open("hashtags.txt", "r", encoding="utf-8")
 for line in tqdm(f):
     hashtag = line[:-1] if line[-1] == '\n' else line
     print('> Parsing ' + hashtag + '...')
 
-    csvFile = open('tweets/' + hashtag[1:] + '.csv', 'w')
+    csvFile = open('tweets/' + hashtag[1:] + '.csv', 'w', encoding="utf-8")
     csvWriter = csv.writer(csvFile)
 
     for tweet in tweepy.Cursor(
