8eaa8163ac55b204e98985a66462fddd456ae181
dobrowj1 <01142560@pw.edu.pl>
2021-01-27 03:28:47 +0100

    Fixed conflicts

--- main.py
+++ main.py
@@ -8,98 +8,98 @@ storage = None
 twitter_api, analytics_api = config.connect_with_services()
 
 
-@app.route('/get-sentiment', methods=['GET', 'POST'])
-def registration_view_post():
-    data = {}
-    data['hashtag'] = '#' + request.form.get("hashtag")
-    data['from'] = request.form.get("from")
+@app.route('/', methods=['GET', 'POST'])
+def sentiment_view():
 
-    errors = {}
-    for k, v in data.items():
-        if not v:
-            errors[k] = "field cannot be empty"
+    if request.method == 'GET':
+        return render_template('welcome.html')
 
-    if errors:
-        return jsonify(errors=errors), 400
+    if request.method == 'POST':
+        data = {}
+        data['hashtag'] = '#' + request.form.get("hashtag")
+        data['from'] = request.form.get("from")
 
-    total_count = 0
-    last_id = 0
-    batch_count = 1
-    tweets_analysed = []
-    # Azure pozwala na wysyłanie maksymalnie 10 dokumentów naraz
-    batch_size = 10
-    positive = 0
-    negative = 0
-    neutral = 0
-    while batch_count > 0 and total_count < 300:
-        if total_count == 0:
-            tweets = tweepy.Cursor(
-                twitter_api.search,
-                tweet_mode='extended',
-                q=data['hashtag'] + ' -filter:retweets',
-                result_type='recent',
-                lang="pl",
-                since=data['from']).items(batch_size)
-        else:
-            tweets = tweepy.Cursor(
-                twitter_api.search,
-                tweet_mode='extended',
-                q=data['hashtag'] + ' -filter:retweets',
-                result_type='recent',
-                lang="pl",
-                max_id=last_id - 1,
-                since=data['from']).items(batch_size)
+        errors = {}
+        for k, v in data.items():
+            if not v:
+                errors[k] = "field cannot be empty"
 
-        batch_count = 0
-        for tweet in tweets:
-            batch_count += 1
-            created_at = str(tweet.created_at).split(' ')[0]
-            last_id = tweet.id
-            tweets_analysed.append({"id": tweet.id_str, "content": tweet.full_text, "created_at": created_at})
+        if errors:
+            return jsonify(errors=errors), 400
 
-        results = None
-        tweets_to_analyse = [tweet["content"] for tweet in tweets_analysed[total_count:total_count + batch_count]]
-        if tweets_to_analyse:
-            results = analytics_api.analyze_sentiment(tweets_to_analyse)
+        total_count = 0
+        last_id = 0
+        batch_count = 1
+        tweets_analysed = []
+        # Azure pozwala na wysyłanie maksymalnie 10 dokumentów naraz
+        batch_size = 10
+        positive = 0
+        negative = 0
+        neutral = 0
+        while batch_count > 0 and total_count < 1000:
+            if total_count == 0:
+                tweets = tweepy.Cursor(
+                    twitter_api.search,
+                    tweet_mode='extended',
+                    q=data['hashtag'] + ' -filter:retweets',
+                    result_type='recent',
+                    lang="pl",
+                    since=data['from']).items(batch_size)
+            else:
+                tweets = tweepy.Cursor(
+                    twitter_api.search,
+                    tweet_mode='extended',
+                    q=data['hashtag'] + ' -filter:retweets',
+                    result_type='recent',
+                    lang="pl",
+                    max_id=last_id - 1,
+                    since=data['from']).items(batch_size)
 
-        i = 0
-        for tweet in tweets_analysed[total_count:total_count + batch_count]:
-            sentiment = results[i]['sentiment']
-            if sentiment == 'positive':
-                positive += 1
-            if sentiment == 'negative':
-                negative += 1
-            if sentiment == 'neutral':
-                neutral += 1
+            batch_count = 0
+            for tweet in tweets:
+                batch_count += 1
+                created_at = str(tweet.created_at).split(' ')[0]
+                last_id = tweet.id
+                tweets_analysed.append({"id": tweet.id_str, "content": tweet.full_text, "created_at": created_at})
 
-            tweet['sentiment'] = sentiment
+            results = None
+            tweets_to_analyse = [tweet["content"] for tweet in tweets_analysed[total_count:total_count + batch_count]]
+            if tweets_to_analyse:
+                results = analytics_api.analyze_sentiment(tweets_to_analyse)
 
-            i += 1
+            i = 0
+            for tweet in tweets_analysed[total_count:total_count + batch_count]:
+                sentiment = results[i]['sentiment']
+                if sentiment == 'positive':
+                    positive += 1
+                if sentiment == 'negative':
+                    negative += 1
+                if sentiment == 'neutral':
+                    neutral += 1
 
-        total_count += batch_count
+                tweet['sentiment'] = sentiment
 
-        avg_sentiment = None
-        if max([positive, negative, neutral]) == positive:
-            avg_sentiment = 'positive'
-        if max([positive, negative, neutral]) == negative:
-            avg_sentiment = 'negative'
-        if max([positive, negative, neutral]) == neutral:
-            avg_sentiment = 'neutral'
+                i += 1
+
+            total_count += batch_count
 
         if total_count == 0:
             return render_template('welcome.html', message="Nie ma tweetów z takim hashtagiem!")
+
+        # avg_sentiment = None
+        # if max([positive, negative, neutral]) == positive:
+        #     avg_sentiment = 'positive'
+        # if max([positive, negative, neutral]) == negative:
+        #     avg_sentiment = 'negative'
+        # if max([positive, negative, neutral]) == neutral:
+        #     avg_sentiment = 'neutral'
 
         positive_percent = round(positive / total_count * 100, 2)
         negative_percent = round(negative / total_count * 100, 2)
         neutral_percent = round(neutral / total_count * 100, 2)
 
-    return render_template('welcome.html', tweets=tweets_analysed[:10], positive_percent=positive_percent,
-            negative_percent=negative_percent, neutral_percent=neutral_percent, total_count=total_count)
-
-
-@app.route('/')
-def welcome():
-    return render_template('welcome.html')
+        return render_template('welcome.html', tweets=tweets_analysed[:10], positive_percent=positive_percent,
+                negative_percent=negative_percent, neutral_percent=neutral_percent, total_count=total_count)
 
 
 if __name__ == '__main__':
